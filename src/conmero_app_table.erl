0d6657ce588d6a565771d9bcd31dcf202547fc28
郑旭进 <xujinzheng@gmail.com>
2013-05-31 23:19:24 +0800

    update header file ref style

--- src/conmero_app_table.erl
+++ src/conmero_app_table.erl
@@ -18,7 +18,7 @@
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--include("../include/conmero.hrl").
+-include("conmero.hrl").
 
 %% ====================================================================
 %% API functions
